235bab569bdf0a929993617ad38c026a31ebf30e
Yazdan Ranjbar <Yazdan_ra@icloud.com>
2021-08-02 05:52:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << 4 + 3 * 5 << endl;
+    cout << (4 + 3) * 5 << endl;
+    cout << 4 * 5 % 3 + 2 << endl;
+    cout << (4 * (5 % 3) + 2) << endl;
+    return 0;
+}
